e7405defcb684d27aae579e42a621b80703dbb10
Javier Oliver <soyjavy@gmail.com>
2016-04-24 23:07:06 +0200

    adding ignore

--- code/Debug/Src/osd.d
+++
@@ -1,6 +0,0 @@
-Src/osd.o: ../Src/osd.c /home/javy/Proyectos/STM32Tris/code/Inc/osd_api.h \
- /home/javy/Proyectos/STM32Tris/code/Inc/game.h
-
-/home/javy/Proyectos/STM32Tris/code/Inc/osd_api.h:
-
-/home/javy/Proyectos/STM32Tris/code/Inc/game.h:
